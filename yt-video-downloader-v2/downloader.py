0ade05cb0f55cee3937bb305a3aae5205f834381
mightybear246 <84099461+mightybear246@users.noreply.github.com>
2024-08-26 22:57:40 +0100

    Version 1.1

--- yt-video-downloader-v2/downloader.py
+++ yt-video-downloader-v2/downloader.py
@@ -6,12 +6,29 @@ pytubefix.request.default_range_size = 50000 # smaller chunk = more progress upd
 
 fileSize = 0.0
 
+videosToBeDownloaded = 0
+videosThatHaveBeenDownloaded = 0
+
 # contradiction: you did something
 def doNothing():
     pass
 
 def get_filesize():
     return fileSize
+
+def get_vidsToBeDownloaded():
+    return videosToBeDownloaded
+
+def set_vidsToBeDownloaded(val):
+    global videosToBeDownloaded
+    videosToBeDownloaded = val
+
+def get_vidsThatHaveBeenDownloaded():
+    return videosThatHaveBeenDownloaded
+
+def set_vidsThatHaveBeenDownloaded(val):
+    global videosThatHaveBeenDownloaded
+    videosThatHaveBeenDownloaded = val
 
 # getting basic information and other shenannigans go here
 
@@ -22,45 +39,53 @@ def get_yt_video(link, progressCallback=doNothing, completeCallback=doNothing) -
 
 # downloading the video, two functions for either using a youtueb obj or vid link
 
-def download_video(link: str, outputPath, progressCallback=doNothing, completeCallback=doNothing):
+def download_video(link: str, outputPath, progressCallback=doNothing, completeCallback=doNothing, resolution='360p'):
     """Download a video using a link"""
     global fileSize
-    fileSize = get_yt_video(link).streams.first().filesize
+    global videosThatHaveBeenDownloaded
+    fileSize = get_yt_video(link).streams.filter(resolution=resolution).first().filesize
     print(fileSize)
     print(get_yt_video(link).title)
-    get_yt_video(link, progressCallback=progressCallback, completeCallback=completeCallback).streams.first().download(output_path=outputPath)
+    get_yt_video(link, progressCallback=progressCallback, completeCallback=completeCallback).streams.filter(resolution=resolution).first().download(output_path=outputPath)
+    videosThatHaveBeenDownloaded += 1
 
 
-def download_video_ytobj(ytObj: YouTube, outputPath, progressCallback=doNothing, completeCallback=doNothing):
+
+def download_video_ytobj(ytObj: YouTube, outputPath, progressCallback=doNothing, completeCallback=doNothing, resolution='360p'):
     """Download a video using a Youtube object"""
     global fileSize
-    fileSize = ytObj.streams.first().filesize
+    global videosThatHaveBeenDownloaded
+    fileSize = ytObj.streams.filter(resolution=resolution).first().filesize
     print(ytObj.title)
     print(fileSize)
     ytObj.register_on_progress_callback(progressCallback)
     ytObj.register_on_complete_callback(completeCallback)
-    ytObj.streams.first().download(output_path=outputPath)
+    ytObj.streams.filter(resolution=resolution).first().download(output_path=outputPath)
+    videosThatHaveBeenDownloaded += 1
 
 # downloading audio, two functionz for either using youbtbe obj or video link
 
 def download_audio(link: str, outputPath, progressCallback=doNothing, completeCallback=doNothing):
     """Downaloadio audio using a link"""
     global fileSize
+    global videosThatHaveBeenDownloaded
     fileSize = get_yt_video(link).streams.filter(only_audio=True).first().filesize
     print(fileSize)
     print(get_yt_video(link).title)
     get_yt_video(link, progressCallback=progressCallback, completeCallback=completeCallback).streams.filter(only_audio=True).first().download(output_path=outputPath, mp3=True)
-
+    videosThatHaveBeenDownloaded += 1
 
 def download_audio_ytobj(ytObj: YouTube, outputPath, progressCallback=doNothing, completeCallback=doNothing):
     """Download audio using a yotuooobe link"""
     global fileSize
+    global videosThatHaveBeenDownloaded
     fileSize = ytObj.streams.filter(only_audio=True).first().filesize
     print(fileSize)
     print(ytObj.title)
     ytObj.register_on_progress_callback(progressCallback)
     ytObj.register_on_complete_callback(completeCallback)
     ytObj.streams.filter(only_audio=True).first().download(output_path=outputPath, mp3=True)
+    videosThatHaveBeenDownloaded += 1
 
 # downloading playlists
 
@@ -80,15 +105,17 @@ def make_dir_for_playlist(link, outputPath):
     return newPath
 # please finish this tommorow
 
-def download_playlist_video(link, outputPath, progressCallback=doNothing, completeCallback=doNothing):
+def download_playlist_video(link, outputPath, progressCallback=doNothing, completeCallback=doNothing, resolution='360p'):
     """Download an entire playlist of videos"""
+    set_vidsToBeDownloaded(get_yt_playlist(link).length)
     filepath =  make_dir_for_playlist(link, outputPath)
     for video in get_yt_playlist(link).videos:
-        download_video_ytobj(ytObj=video, outputPath=filepath, progressCallback=progressCallback, completeCallback=completeCallback)
+        download_video_ytobj(ytObj=video, outputPath=filepath, progressCallback=progressCallback, completeCallback=completeCallback, resolution=resolution)
 
 
 def download_playlist_audio(link, outputPath, progressCallback=doNothing, completeCallback=doNothing):
     """Download an enitre playlist of audio"""
+    set_vidsToBeDownloaded(get_yt_playlist(link).length)
     filepath =  make_dir_for_playlist(link, outputPath)
     for video in get_yt_playlist(link).videos:
         download_audio_ytobj(ytObj=video, outputPath=filepath, progressCallback=progressCallback, completeCallback=completeCallback)
